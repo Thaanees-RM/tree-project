25f7562cce41f928f4be476947847de550c5b88b
Fayaz <96859481+fayazamd@users.noreply.github.com>
2025-07-09 10:46:34 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import Join from "./components/JoinForm";
+import Upload from "./components/joinFormStep2";
+import Submit from "./components/joinFormStep3";
 
 const App = () => {
   return (
     <>
       <Header/>
       <Routes>
+        
         <Route path="/home" element={<Home />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy/>} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/join/upload" element={<Upload />} />
+        <Route path="/join/submit" element={<Submit />} />
       </Routes>      
       <Footer/>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,6 +71,7 @@
 // export default Header
 
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -84,7 +85,10 @@ const Header = () => {
         </div>
         <div className='flex flex-row gap-6 items-center'>
           <a href="privacy-policy" className='font-medium text-base text-black'>Privacy Policy</a>
-          <button className='font-medium text-base text-white bg-[#006A34] w-20 py-2 rounded-sm'>Join</button>
+          <Link to="/join">
+            <button className='font-medium text-base text-white bg-[#006A34] w-20 py-2 rounded-sm cursor-pointer'>Join</button>
+          </Link>
+          
         </div>
       </header>
 
@@ -93,7 +97,10 @@ const Header = () => {
         <a href="home" className='font-bold text-2xl text-[#006A34]'>LOGO</a>
 
         <div className='flex items-center gap-4'>
-          <button className='font-medium text-base text-white bg-[#006A34] w-20 py-2 rounded-sm'>Join</button>
+          <Link to="/join">
+            <button className='font-medium text-base text-white bg-[#006A34] w-20 py-2 rounded-sm'>Join</button>
+          </Link>
+          
 
           {/* Hamburger Button */}
           <button
@@ -133,7 +140,11 @@ const Header = () => {
           </div>
           <div className='flex flex-col gap-5 items-center justify-center h-[60%]'>
             <a href="privacy-policy" className='font-medium text-lg text-black'>Privacy Policy</a>
-            <button className='font-medium text-base text-white bg-[#006A34] w-34 py-2 rounded-sm'>Join</button>
+            <Link to="/join">
+              <button className='font-medium text-base text-white bg-[#006A34] w-34 py-2 rounded-sm'>
+                Join
+              </button>
+            </Link>
           </div>
         </div>
       )}
