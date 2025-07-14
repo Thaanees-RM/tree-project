139ab0c154002ae70cb91544657fd1e2cf7f3582
SRI <148487229+Srisajeenthran@users.noreply.github.com>
2025-07-14 11:59:23 +0530

    small changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { findUserByName } = require("../models/User");
+const { findUserByName } = require("../models/auth");
 
 const login = async (req, res) => {
   const { name, password } = req.body;
